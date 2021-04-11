4d6083706a29c8a7213e5b4da4258cd3265fe958
qjy415417122 <73819322+qjy415417122@users.noreply.github.com>
2021-04-11 17:54:35 +0800

    Update util.py

--- util.py
+++ util.py
@@ -1,17 +1,44 @@
 import math
 import numpy as np
 
-def pyu(y,u,theta,A):
-    temp1 = (y-A@u).T@(y - A@u)/theta**2
-    temp2 = u.T @ u/theta**2
-    Phi = (temp1+temp2)*(-0.5)
+
+def pyu(y, u, A, sigama_1, sigama_2):
+    temp1 = (y - A @ u).T @ np.linalg.inv(sigama_1) @ (y - A @ u)
+    temp2 = u.T @ np.linalg.inv(sigama_2) @ u
+    Phi = (temp1 + temp2) * (-0.5)
     return Phi
 
 
+def prior_cov(height, width, gamma, d):
+    """
+    K(xy_i,xy_j) = gamma*exp(-||(x_i,y_i)-(x_j,y_j)||_2/d)
+    """
+    xs = np.linspace(0, 1, width + 1)
+    delta_x = (xs[1] - xs[0]) / 2
+    xs = xs[:-1] + delta_x
+
+    ys = np.linspace(0, 1, height + 1)
+    ys = ys[:-1] + delta_x
+
+    [X, Y] = np.meshgrid(xs, ys)
+    XYpoint = np.c_[X.flatten(), Y.flatten()]
+
+    n = height * width
+    dist_upper = np.zeros((n, n))
+    for i in range(n):
+        for j in range(i, n):
+            dist_upper[i, j] = np.linalg.norm(XYpoint[i, :] - XYpoint[j, :])
+    dist = dist_upper + dist_upper.T
+    K = gamma * np.exp(-dist / d)
+    return K
+
+
 def sigmoid(x):
-    return 1.0/(1+np.exp(-x))
+    return 1.0 / (1 + np.exp(-x))
+
+
 def cauchy(x):
-    y=((math.e ** (-((x - 2) ** 2) / 0.5) / math.sqrt(2 * math.pi * 0.5)) + (
+    y = ((math.e ** (-((x - 2) ** 2) / 0.5) / math.sqrt(2 * math.pi * 0.5)) + (
             math.e ** (-((x - 3) ** 2) / 0.18) / math.sqrt(2 * math.pi * 0.3))) / 2
     return y
 
