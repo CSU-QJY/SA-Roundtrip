dbdae03c778993936ea45e8864e77d142b0ba464
qjy415417122 <73819322+qjy415417122@users.noreply.github.com>
2022-12-13 13:21:57 +0800

    Delete test_3 directory

--- test_3/TF2_Roundtrip/model_test.py
+++
@@ -1,8 +0,0 @@
-from tensorflow import keras
-import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
-model_G = keras.models.load_model('Roundtrip_model_G.h5', compile=False)
-test=model_G.predict(np.array(tf.random.normal(shape=(1, 110))))
-plt.imshow(test.reshape([28,28]))
-plt.show()
